5c8ab1697ce9d2e4e02ddd3f599263a68bf94188
Артемий Винн <hoztov25@ya.ru>
2023-03-10 16:16:39 +0300

    First commit34

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 CSRF_TRUSTED_ORIGINS = ["http://158.160.48.13/*", "http://*", "https://*"]
 
